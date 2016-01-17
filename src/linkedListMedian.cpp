7b5517f1203a6bc635e9c23c23a6f608769ff770
shilpathakur95 <shilpathakur95@missionrnd.com>
2016-01-17 13:50:15 +0530

    Submit Commit

--- src/linkedListMedian.cpp
+++ src/linkedListMedian.cpp
@@ -20,5 +20,44 @@ struct node {
 };
 
 int linkedListMedian(struct node *head) {
+	if (head==NULL)
 	return -1;
+	struct node *front = head, *back = head;
+	struct node *temp = NULL;
+	int flag, count = 0;
+	float med;
+	while (front)
+	{
+		if (front->next == NULL)
+		{
+			flag = 1;
+			break;
+		}
+		else if (front->next->next == NULL)
+		{
+			flag = 0;
+			break;
+		}
+		else
+		{
+			front = front->next->next;
+			count++;
+		}
+	}
+	while (count)
+	{
+		back = back->next;
+		count--;
+	}
+	if (flag == 1)
+	{
+		return back->num;
+	}
+	else if (flag == 0)
+	{
+		temp = back;
+		back = back->next;
+		med = (temp->num + back->num) / 2;
+		return med;
+	}
 }
