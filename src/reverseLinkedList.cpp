f6832564688e3012493f408d915ad537976a136b
shilpathakur95 <shilpathakur95@missionrnd.com>
2016-01-17 13:45:12 +0530

    Submit Commit

--- src/reverseLinkedList.cpp
+++ src/reverseLinkedList.cpp
@@ -12,12 +12,39 @@ NOTES:
 */
 
 #include <stdio.h>
-
+void find(struct node *head);
 struct node {
 	int num;
 	struct node *next;
 };
 
 struct node * reverseLinkedList(struct node *head) {
+	if (head==NULL)
 	return NULL;
+	struct node *temp2 = head;
+	while (temp2->next != NULL)
+	{
+		temp2 = temp2->next;
+	}
+	find(head);
+	head = temp2;
+	return head;
+}
+void find(struct node *head)
+{
+	struct node *temp = head;
+	struct node *prev = NULL;
+	while (temp->next != NULL)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+	if (prev != NULL)
+	{
+		temp->next = prev;
+		prev->next = NULL;
+		find(head);
+	}
+	else
+		temp->next = prev;
 }
